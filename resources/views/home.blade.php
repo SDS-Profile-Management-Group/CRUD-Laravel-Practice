e88761f00789244f83c989e64e702e1e6799b5c8
Afwan-Rezal <21b6027@ubd.edu.bn>
2024-09-10 22:04:23 +0800

    Added post blog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,16 @@
         <button>Log out</button>
     </form>
 
+    <div style="border: 3px solid black">
+        <h2>Create a new post</h2>
+        <form action="/create-post" method="POST">
+            @csrf
+            <input type="text" name="title" placeholder="Post title">
+            <textarea name="body" placeholder="Body Content..."></textarea>
+            <button>Save Post</button>
+        </form>
+    </div>
+
     @else
     <div style="border: 3px solid black">
         <h2>Register</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +9,8 @@ Route::get('/', function () {
 });
 
 Route::post('/register', [UserController::class, 'register']);
-
 Route::post('/logout', [UserController::class,'logout']);
-
 Route::post('/login', [UserController::class,'login']);
+
+
+Route::post('/create-post', [PostController::class,'createPost']);
